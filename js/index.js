03e3d812214a6534698dc25d4579eee2f88d10d7
Karadeg Daucé <karadeg.dauce@reseau.eseo.fr>
2022-12-17 22:29:32 +0100

    début de l'animation d'accueil

---
+++ js/index.js
@@ -0,0 +1,68 @@
+document.getElementById('click').addEventListener('click', () => {
+    startAnimationHomepage();
+});
+
+const cursor = document.createElement('div');
+cursor.style.cssText = 'width:100px;height:100px;border-radius:100%;background:white;position:absolute;transform:translate(-50%,-50%);mix-blend-mode: difference;pointer-events: none;';
+cursor.classList.add('cursor');
+document.body.appendChild(cursor);
+
+document.addEventListener('mousemove', (event) => {
+    // récupère la position du curseur
+    const x = event.clientX;
+    const y = event.clientY;
+  
+    // déplace la div pour qu'elle suive le curseur
+    cursor.style.top = y + 'px';
+    cursor.style.left = x + 'px';
+});
+
+function startAnimationHomepage () {
+    document.getElementById('click_container').classList.add('fadeOut');
+    setTimeout(function(){
+        document.getElementById('title_homepage').classList.add('moveClipPath');
+    }, 800);
+    setTimeout(function(){
+        document.getElementById('divider').classList.add('moveClipPath');
+    }, 1000);
+    setTimeout(function(){
+        document.getElementById('description').classList.add('moveClipPath');
+    }, 1300);
+    setTimeout(function(){
+        document.getElementById('divider').classList.add('moveClipPath');
+    }, 1000);
+    setTimeout(function(){
+        document.getElementById('title_homepage').classList.remove('moveClipPath');
+        document.getElementById('title_homepage').classList.add('movedClipPath');
+    }, 2300);
+    setTimeout(function(){
+        document.getElementById('divider').classList.remove('moveClipPath');
+        document.getElementById('divider').classList.add('movedClipPath');
+    }, 2500);
+    setTimeout(function(){
+        document.getElementById('description').classList.remove('moveClipPath');
+        document.getElementById('description').classList.add('movedClipPath');
+    }, 2800);
+    setTimeout(function(){
+        document.getElementById('frame_2').classList.add('slideOut');
+    }, 3000);
+    setTimeout(function(){
+        document.getElementById('frame_3').classList.add('slideOut');
+    }, 3200);
+    setTimeout(function(){
+        document.getElementById('frame_4').classList.add('slideOut');
+    }, 3400);
+    setTimeout(function(){
+        document.getElementById('frame_5').classList.add('slideOut');
+    }, 3600);
+    setTimeout(function(){
+        document.getElementById('frame_6').classList.add('slideOut');
+    }, 3800);
+    setTimeout(function(){
+        document.getElementById('frame_7').classList.add('slideIn');
+    }, 4000);
+    
+}
+  
+  
+  
